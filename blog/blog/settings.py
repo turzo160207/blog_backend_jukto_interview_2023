edfc6efc64c9829d6530a039d47f39f0de281f45
turzo160207 <42179677+turzo160207@users.noreply.github.com>
2023-03-21 01:25:38 +0600

    db engine switched mysql

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,10 +75,24 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "db",
+        'USER': 'root',
+        'PASSWORD': '456456456',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
